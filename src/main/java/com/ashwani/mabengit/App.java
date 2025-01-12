53a47ab95214c09a8cef0ad79e4995c2334031ff
Ashwanichandel <ashwanichandel93964@gmail.com>
2025-01-12 13:58:54 +0530

    seanson class created

--- src/main/java/com/ashwani/mabengit/App.java
+++ src/main/java/com/ashwani/mabengit/App.java
@@ -6,5 +6,7 @@ package com.ashwani.mabengit;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Season s= new Season(100);
+        System.out.println(s);
     }
 }
---
+++ src/main/java/com/ashwani/mabengit/Season.java
@@ -0,0 +1,23 @@
+package com.ashwani.mabengit;
+
+public class Season {
+	public int x;
+
+	public Season(int x) {
+		this.x = x;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	@Override
+	public String toString() {
+		return "Season [x=" + x + "]";
+	}
+
+}
